5f2a1c19c834e5f5ff1ef7ca6472052cccac4832
Vinicius Melo <vmelo_@hotmail.com>
2026-09-19 21:42:22 -0400

    Prepare frontend for deployment

--- src/MeloSupportDesk.Web/src/api/client.ts
+++ src/MeloSupportDesk.Web/src/api/client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5159/api',
+baseURL:
+  import.meta.env.VITE_API_URL ?? 'http://localhost:5159/api',
   headers: {
     'Content-Type': 'application/json',
   },
