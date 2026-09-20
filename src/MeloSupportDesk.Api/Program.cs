86f3a5d8459fc087176679f1354959d1d01d4cea
Vinicius Melo <vmelo_@hotmail.com>
2026-09-20 19:51:57 -0400

    Allow Vercel frontend origin

--- src/MeloSupportDesk.Api/Program.cs
+++ src/MeloSupportDesk.Api/Program.cs
@@ -92,7 +92,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("http://localhost:5173", "https://melo-support-desk.vercel.app")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
